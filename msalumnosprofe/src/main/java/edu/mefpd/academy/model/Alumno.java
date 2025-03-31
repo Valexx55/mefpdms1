98b877e6a8e6c0709eba78f7397a1442cafc5897
vale <valexx55@gmai.com>
2025-03-31 13:51:01 +0200

    Alumno entity

--- msalumnosprofe/src/main/java/edu/mefpd/academy/model/Alumno.java
+++ msalumnosprofe/src/main/java/edu/mefpd/academy/model/Alumno.java
@@ -1,9 +1,36 @@
 package edu.mefpd.academy.model;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "alumnos")
 public class Alumno {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //auto inc en mysql
+	private Long id;
 	
 	private String nombre;
 	private int edad;
+	private String email;
+	private String apellido;
+	
+	@Column(name = "creado_en")
+	private LocalDateTime creadoEn;
+	
+	@PrePersist
+	private void generarFechaCreacion ()
+	{
+		this.creadoEn = LocalDateTime.now();//obtengo la fecha de creación
+	}
 	
 	public Alumno() {
 		super();
@@ -15,6 +42,18 @@ public class Alumno {
 		this.edad = edad;
 	}
 	
+	
+	
+	public Alumno(Long id, String nombre, int edad, String email, String apellido, LocalDateTime creadoEn) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.edad = edad;
+		this.email = email;
+		this.apellido = apellido;
+		this.creadoEn = creadoEn;
+	}
+
 	public String getNombre() {
 		return nombre;
 	}
@@ -26,6 +65,38 @@ public class Alumno {
 	}
 	public void setEdad(int edad) {
 		this.edad = edad;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public LocalDateTime getCreadoEn() {
+		return creadoEn;
+	}
+
+	public void setCreadoEn(LocalDateTime creadoEn) {
+		this.creadoEn = creadoEn;
 	}
 	
 	
