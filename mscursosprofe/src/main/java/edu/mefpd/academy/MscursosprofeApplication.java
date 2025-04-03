2fc6c3a39d23e69f22e71a41baafc3a81cf27587
vale <valexx55@gmai.com>
2025-04-03 10:59:00 +0200

    rest curso 1/2

---
+++ mscursosprofe/src/main/java/edu/mefpd/academy/MscursosprofeApplication.java
@@ -0,0 +1,13 @@
+package edu.mefpd.academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscursosprofeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscursosprofeApplication.class, args);
+	}
+
+}
---
+++ mscursosprofe/src/main/java/edu/mefpd/academy/model/Curso.java
@@ -0,0 +1,10 @@
+package edu.mefpd.academy.model;
+
+public class Curso {
+	
+	private Long id;
+	private String nombre;
+	
+	//TODO: terminar de definir las capas del Servicio REST
+
+}
